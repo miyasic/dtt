f2d98a89c16d305439e09a32ee4e85398a3b9492
miyaji555 <kou.sepak@gmail.com>
2024-03-10 12:31:06 +0900

    delete unused files

--- example/dart_test_tools_example.dart
+++
@@ -1,6 +0,0 @@
-import 'package:dart_test_tools/dart_test_tools.dart';
-
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
--- lib/dart_test_tools.dart
+++ lib/dart_test_tools.dart
@@ -3,6 +3,4 @@
 /// More dartdocs go here.
 library;
 
-export 'src/dart_test_tools_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+export 'src/command_runner.dart';
